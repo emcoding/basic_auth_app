cff48f0e3a9914df2012d1447f61d5aea3a47d33
F3PiX <F3PiXnl@gmail.com>
2015-12-26 21:22:44 +0100

    Add sign up functionality
    Added a users_controller to create new user w strong params.
    Added a new view with form f signing up
    Added a root route.
    Signup functionality is done, now to the log in.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_url, notice: "Welkom! Je account is klaar"
+    else
+      render "new" #todo change to better ending
+    end
+  end
+
+
+private
+
+ def user_params
+   params.require(:user).permit(:email, :password, :password_confirmation)
+ end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+
+  root to: 'users#new'
   resources :users
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
