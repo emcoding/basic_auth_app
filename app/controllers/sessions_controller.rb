8eacf14fe9d2ab79fd101f19ec724df16edcf450
F3PiX <F3PiXnl@gmail.com>
2015-12-27 10:56:24 +0100

    Add logout functionality
    A logged in user may want to log out = destroy current session. Added :delete session method and added log out link to view for logged in user only

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,4 +15,9 @@ class SessionsController < ApplicationController
       render "new"
     end
   end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: "Je bent uitgelogd"
+  end
 end
