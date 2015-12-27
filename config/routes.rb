bb94ae96e21683daf517c971e4690c0ff6ff463e
F3PiX <F3PiXnl@gmail.com>
2015-12-27 11:44:44 +0100

    Change to pretty route names
    Changed route names, and changed links in views accordingly.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'inloggen', to: 'sessions#new', as: 'inloggen'
+  get 'aanmelden', to: 'users#new', as: 'aanmelden'
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
+
   root to: 'users#new'
   resources :users
   resources :sessions
