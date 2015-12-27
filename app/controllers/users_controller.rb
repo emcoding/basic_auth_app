29bc4368272e3c2ec268a6d44b40dbb7b6f32aa4
F3PiX <F3PiXnl@gmail.com>
2015-12-27 12:04:00 +0100

    Make new signed up user automatically logged in
    Add new session to user#create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_url, notice: "Welkom! Je account is klaar"
     else
       render "new" #todo change to better ending
