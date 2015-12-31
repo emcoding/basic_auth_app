2040da0cb9f9aa77c74a611b36fa0ff7bf7f44b7
F3PiX <F3PiXnl@gmail.com>
2015-12-31 15:44:03 +0100

    Add show User#show view, remove redundant routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
+  root to: 'sessions#new'
+
   get 'inloggen', to: 'sessions#new', as: 'inloggen'
   get 'aanmelden', to: 'users#new', as: 'aanmelden'
   delete 'logout', to: 'sessions#destroy', as: 'logout'
 
-  root to: 'users#new'
-  resources :users
-  resources :sessions
+  resources :users, only: [:new, :show, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
 end
